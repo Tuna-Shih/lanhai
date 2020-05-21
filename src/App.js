c29be55b0e45fee3300ce4a980451b3cf5684fb6
Tuna-Shih <61424827+Tuna-Shih@users.noreply.github.com>
2020-05-22 00:02:06 +0800

    Infinite scroll (#23)
    * style: Todo=>TodoList
    
    * feat: scroll
    
    * feat: uuid (#19)
    
    * feat: uuid
    
    use uuid instead of id++
    
    * feat: uuid
    
    * refactor: remove this.id
    
    * feat: scroll
    
    * feat: json-server
    
    * feat: scroll
    
    * feat: scroll
    
    前面的不小心merge到舊的
    
    * feat: scroll with js-cookie
    
    * feat: autoload
    
    * feat: first after
    
    * fix: startindex
    
    * refactor: remove unuse

--- src/App.js
+++ src/App.js
@@ -12,34 +12,49 @@ class App extends React.Component {
   };
 
   componentDidMount() {
-    const todoData = cookies.get('todoapp');
-    if (todoData) {
-      const oldTodos = JSON.parse(todoData);
-      this.setState({
-        todos: oldTodos
-      });
-    }
+    this.autoLoad();
+
+    document.addEventListener('scroll', () => {
+      if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+        this.loadMore();
+      }
+    });
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.todos !== this.state.todos) {
-      cookies.set('todoapp', JSON.stringify(this.state.todos));
-    }
-  }
   addTodo = () => {
     const { todos, todoText } = this.state;
     if (todoText.replace(/\s*/g, '') !== '') {
+      const todo = { id: uuidv4(), text: todoText };
+      const newTodos = [todo, ...todos];
+
       this.setState({
-        todos: [...todos, { id: uuidv4(), text: todoText }],
+        todos: newTodos,
         todoText: ''
       });
+      cookies.set('todoapp', JSON.stringify(newTodos));
     }
   };
 
   deleteTodo = id => {
+    const { todos } = this.state;
+    const newTodos = todos.filter(todo => todo.id !== id);
+
     this.setState({
-      todos: this.state.todos.filter(todo => todo.id !== id)
+      todos: newTodos
     });
+    cookies.set('todoapp', JSON.stringify(newTodos));
+  };
+
+  editTodo = (id, edit) => {
+    const { todos } = this.state;
+    const newTodos = todos.map(todo =>
+      todo.id === id && edit.text.replace(/\s*/g, '') !== '' ? edit : todo
+    );
+
+    this.setState({
+      todos: newTodos
+    });
+    cookies.set('todoapp', JSON.stringify(newTodos));
   };
 
   handleChange = e => {
@@ -48,18 +63,44 @@ class App extends React.Component {
     });
   };
 
-  editTodo = (id, edit) => {
-    this.setState({
-      todos: this.state.todos.map(todo =>
-        todo.id === id && edit.text.replace(/\s*/g, '') !== '' ? edit : todo
-      )
+  loadData = (first, after) => {
+    const getData = cookies.get('todoapp');
+
+    if (!getData) return [];
+
+    const todoData = JSON.parse(getData);
+
+    const startIndex = !after
+      ? -1
+      : todoData.findIndex(todo => todo.id === after);
+
+    return todoData.slice(startIndex + 1, startIndex + first);
+  };
+
+  loadMore = callback => {
+    const { endCursor, todos } = this.state;
+    const gotData = this.loadData(10, endCursor);
+
+    if (gotData.length === 0) return;
+    const lastID = gotData.length - 1;
+    const newTodo = [...todos, ...gotData];
+    this.setState({ endCursor: gotData[lastID].id, todos: newTodo }, () => {
+      if (!callback) return;
+      callback();
     });
+  };
+
+  autoLoad = () => {
+    if (window.innerHeight < document.body.scrollHeight) return;
+    this.loadMore(this.autoLoad);
   };
 
   render() {
     const { todos, todoText } = this.state;
+
     return (
       <div className="wrapper">
+        <button onClick={this.loadMore}>XD</button>
         <div className="add">
           <TodoItem
             todoText={todoText}
@@ -67,7 +108,7 @@ class App extends React.Component {
             handleChange={this.handleChange}
           />
         </div>
-        <h2>Todo!</h2>
+        <h2> {todos.length} Todo! </h2>
         <div className="list">
           {todos.map(todo => (
             <Todo
