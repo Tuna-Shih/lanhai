9a27f72d3d986da48217617a6ed637784d37e1cc
Tuna-Shih <61424827+Tuna-Shih@users.noreply.github.com>
2020-06-01 13:40:49 +0800

    feat: init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout, Menu } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
+const { Header, Content, Footer } = Layout;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header
+        style={{
+          position: 'fixed',
+          zIndex: 1,
+          width: '100%'
+        }}>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ fontSize: 32, backgroundColor: 'blue', opacity: 50 }}>
+          <Menu.Item key="1">Home</Menu.Item>
+          <Menu.Item key="2">來電洽詢</Menu.Item>
+          <Menu.Item key="3">立即預約</Menu.Item>
+        </Menu>
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: '0 50px', marginTop: 64, backgroundColor: 'black' }}>
+        <div
+          className="site-layout-background"
+          style={{ padding: 24, minHeight: 1000 }}>
+          XDXDXD
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
   );
 }
 
