564b2fc1f3d9a8bb9c658034aa9ad9b434ab0015
Tuna-Shih <61424827+Tuna-Shih@users.noreply.github.com>
2020-06-17 22:34:55 +0800

    feat: validation (#30)
    * feat: validation
    
    * feat: /[^x00-xff]/g
    
    * feat: validate
    
    * feat: modify validate
    
    * feat: some()
    
    * feat: remove unuse

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles/TodoItem.less';
 import { Button, Input } from 'antd';
+import stringWidth from 'string-width';
 
 class TodoItem extends React.Component {
+  handleVaildation = () => {
+    const { todoText, addTodo } = this.props;
+
+    if (
+      todoText.length >= 200 ||
+      todoText.split('').some(e => stringWidth(e) == 2)
+    )
+      return alert('Illegal input');
+    addTodo();
+  };
+
   render() {
-    const { todoText, addTodo, deleteAllTodo, handleChange } = this.props;
+    const { todoText, deleteAllTodo, handleChange } = this.props;
     return (
       <div className={styles.add_item}>
         <Input
@@ -14,7 +26,7 @@ class TodoItem extends React.Component {
           onChange={handleChange}
           placeholder="Add Something"
         />
-        <Button type="primary" onClick={addTodo}>
+        <Button type="primary" onClick={this.handleVaildation}>
           New
         </Button>
         <Button type="primary" onClick={deleteAllTodo}>
